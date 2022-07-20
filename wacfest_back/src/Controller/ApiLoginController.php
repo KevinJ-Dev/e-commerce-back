2872b8f029619d85b1c977b4a88d9e10a69eb2cf
Fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-07-20 16:22:04 +0200

    Add Controller login/register

---
+++ wacfest_back/src/Controller/ApiLoginController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controller;
+
+ use App\Entity\Users;
+use App\Security\LoginAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+ class ApiLoginController extends AbstractController
+ {
+
+
+
+    #[Route('/login', name: 'login')]
+
+    public function index() {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiLoginController.php',
+           
+        ]); 
+    }
+
+
+    #[Route('/login', name: 'login', methods: ['POST'])]
+
+
+     public function login( LoginAuthenticator $auth) : Response
+     {
+
+            $this->auth($auth);
+            return $this->json([
+            'message' => 'Logged in !',
+            'path' => 'src/Controller/ApiLoginController.php',
+            'token' => "dslmfkmldfk",
+           
+        ]);
+     }
+
+
+     private function auth() {
+
+     }
+ }
---
+++ wacfest_back/src/Controller/RegisterController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'register', methods: ['POST'])]
+
+
+    public function register(Request $request, UserRepository $repo, UserPasswordHasherInterface $passwordHasher) {
+
+        $user = new User();
+        $prenom =  $request->request->get('prenom');
+        $nom =  $request->request->get('nom');
+        $email = $request->request->get('email');
+        $age = intval($request->request->get('age'));
+        $password = $request->request->get("password");
+        $user->setEmail($email);
+        $user->setPrenom($prenom);
+        $user->setNom($nom);
+        $user->setAge($age);
+        $pass = $request->request->get('password');
+        $password = $passwordHasher->hashPassword( $user ,$pass);
+        $user->setPassword($password);
+        // $user->setPassword(PASSWORD_BCRYPT, $password);
+        $user->setRoles(['user'=>$user]);
+        $repo->add($user, true);
+
+
+        return $this->redirectToRoute('login', [
+           "pseudo" => $user->getEmail(),
+           "email" => $user->getEmail(),
+            "age" => $user->getAge(),
+            "password" => $user->getPassword(),
+
+        ]);
+
+
+
+    }
+
+    public function validator() {
+        return "";
+    }
+}

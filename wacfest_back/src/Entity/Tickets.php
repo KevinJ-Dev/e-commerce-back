b5dee439195d4ed63fa6e0de554eedad73a24b80
nalor <orlane.margueritte@epitech.eu>
2022-07-21 16:41:30 +0200

    Make Tickets/Controller/Entity/Repository/templates

---
+++ wacfest_back/src/Entity/Tickets.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketsRepository::class)]
+class Tickets
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
